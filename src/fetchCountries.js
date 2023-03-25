f91885444cf1f9f8918eebcaa2897afc051d1e2e
Inna Makarenko <macksi@ukr.net>
2023-03-25 23:50:27 +0200

    updated task

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,31 +1,10 @@
 export const fetchCountries = name => {
-  fetch(
+  return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,flags,capital,population,languages`
-  )
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('🚀~ data:', data);
-
-      const {
-        name: { official },
-        flags: { svg: flag },
-        capital,
-        population,
-        languages,
-      } = data[0];
-      console.log(official);
-      console.log(flag);
-      console.log(capital[0]);
-      console.log(population);
-      console.log(Object.values(languages).join(', '));
-      return data;
-    })
-    .catch(console.warn);
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 };
-
-fetchCountries('ukraine');
--- src/index.js
+++ src/index.js
@@ -1,22 +1,41 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-// import fetchCountries from "./fetchCountries";
 import { fetchCountries } from './fetchCountries';
-
+import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
-let name = '';
 
 const countryListEl = document.querySelector('ul.country-list');
 const inputCountryEl = countryListEl.previousElementSibling;
 const countryInfoEl = countryListEl.nextElementSibling;
 
 const handleSearchCounry = e => {
-  name = e.target.value;
-  // fetchCountries(name);
-  console.log('name:', name);
-  //   if (official.includes(name)) {
-  //   markupListCountry()
-  // }
+  fetchCountries(e.target.value.trim())
+    .then(data => {
+      if (data.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (data.length <= 10 && data.length > 1) {
+        countryListEl.innerHTML = '';
+        countryInfoEl.innerHTML = '';
+        data.map(item => {
+          markupListCountry(item);
+        });
+      } else if (data.length === 1) {
+        countryInfoEl.innerHTML = '';
+        countryListEl.innerHTML = '';
+        data.map(item => {
+          markupCountry(item);
+        });
+      }
+    })
+    .catch(error => {
+      if (error.message === '404') {
+        Notiflix.Notify.failure('Oops, there is no country with that name.');
+      } else {
+        console.warn(error);
+      }
+    });
 };
 
 inputCountryEl.addEventListener(
@@ -24,22 +43,27 @@ inputCountryEl.addEventListener(
   debounce(handleSearchCounry, DEBOUNCE_DELAY)
 );
 
-// let country = fetchCountries('ukraine');
-// console.log("🚀 ~ file: index.js:19 ~ country:", country);
+function markupListCountry(item) {
+  countryListEl.insertAdjacentHTML(
+    'beforeend',
+    `<p class="country_name_list">
+    <img src="${item.flags.svg}" 
+    alt="country" class="country_flag" width="50">
+    ${item.name.common}</p>`
+  );
+}
 
-// function markupListCountry() {
-//   countryListEl.insertAdjacentElement(
-//     'afterbegin',
-//     `<p class="country_name_list"><img src="${flag}" alt="country" class="country_flag_list width="100" height="50"">${official}</p>`
-//   );
-// }
-
-// function markupCountry() {
-//   `<p class="country_name"><img src="${flag}" alt="country" class="country_flag width="100" height="50"">${official}</p>
-
-//     <p class="country_capital">Capital: ${capital[0]}</p>
-//     <p class="country_population">Population: ${population}</p>
-//     <p class="country_languages">Languages: ${Object.values(languages).join(
-//       ', '
-//     )}</p>`;
-// }
+function markupCountry(item) {
+  countryInfoEl.insertAdjacentHTML(
+    'afterbegin',
+    `<p class="country_name">
+    <img src="${item.flags.svg}" alt="country" class="country_flag" width="50">
+    ${item.name.common}</p>
+    <p class="country_item"><span class="country_title">Capital:</span> 
+    ${item.capital[0]}</p>
+    <p class="country_item"><span class="country_title">Population:</span> 
+    ${item.population}</p>
+    <p class="country_item"><span class="country_title">Languages:</span>
+    ${Object.values(item.languages).join(', ')}</p>`
+  );
+}
